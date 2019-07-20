ae0a9bbe08eb1a7fd9bbbc71031dcdc75ee6f986
BH Kim <blue021@gmail.com>
2019-07-20 09:35:25 +0900

    initial commit

---
+++ 001.quick_sort.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("vim-go")
+}
+
+func quick_sort([]int) []int {
+
+}
---
+++ 002.merge_sort.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("vim-go")
+}
+
+func merge_sort([]int) []int {
+
+}
